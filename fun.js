7bbe210d48243ea1a0d5955c17c24ec25ab7a7cd
prabin <bhandariprabin402@gmail.com>
2023-12-22 17:18:04 +0545

    add fun1

--- fun.js
+++ fun.js
@@ -1,10 +1,10 @@
 function printPattern(range) {
-  for (var i = 1; i <= range; i++) {
-    var str = "";
-    for (var j = 1; j <= i; j++) {
+  for (let i = 1; i <= range; i++) {
+    let str = "";
+    for (let j = 1; j <= i; j++) {
       str += j + " ";
     }
     console.log(str);
   }
 }
-printPatter(8);
+printPattern(8);
---
+++ fun1.js
@@ -0,0 +1,7 @@
+const a = number(prompt("enter the number"));
+const b = number(prompt("enter number"));
+function multification(a, b) {
+  return a * b;
+}
+const o = multification(a, b);
+console.log(o);
