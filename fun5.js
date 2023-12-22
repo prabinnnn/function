203489a45775e8434bf7a44f7922610466e55b39
prabin <bhandariprabin402@gmail.com>
2023-12-22 18:17:40 +0545

    add fun5

---
+++ fun5.js
@@ -0,0 +1,9 @@
+let i;
+let a = Number(prompt("enter number 3"));
+function multifaction(a) {
+  for (let i = 0; i <= 10; i++) {
+    return a * i;
+  }
+}
+const res = multifaction(a);
+console.log(res);
