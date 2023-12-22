af2afe3fec32e8a9e0c6f903099343e70906ee23
prabin <bhandariprabin402@gmail.com>
2023-12-22 17:52:07 +0545

    add fun3

---
+++ fun3.js
@@ -0,0 +1,6 @@
+let num = Number(prompt("enter number"));
+function checkevennumber(num) {
+  let e =
+    num % 2 === 0 ? console.log("even number") : console.log("ood number");
+}
+checkevennumber(num);
