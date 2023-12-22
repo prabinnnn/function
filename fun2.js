d60046d8c446bad3ecf681debd6716d5748ddfbf
prabin <bhandariprabin402@gmail.com>
2023-12-22 17:42:15 +0545

    add fun2

--- fun2.js
+++ fun2.js
@@ -1,7 +1,5 @@
 let age = prompt("enter the age");
 function myvote(age) {
   let a = age <= 18 ? console.log("u can vote ") : console.log("u cant vote");
-  return a;
 }
-const e = myvote(age);
-console.log(e);
+myvote(age);
